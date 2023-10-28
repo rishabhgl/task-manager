0e6fc9bb1b4d174693ce64c0da918485d681b548
rishabhgl <rishabhisjayden@gmail.com>
2023-10-28 19:37:08 +0530

    Fetched documents

--- mongodb.js
+++ mongodb.js
@@ -1,16 +1,22 @@
-const MongoClient = require("mongodb").MongoClient
+const { MongoClient, ObjectId } = require("mongodb")
 
 const dbURL = "mongodb://127.0.0.1:27017"
 const client = new MongoClient(dbURL)
-const dbName = 'task-manager'
 
-async function connectToDBServer(){
+async function insertDocs(){
     await client.connect()
     await client.db("admin").command({ ping: 1 })
     console.log('Connection established and tested')  
     
-    const db = client.db(dbName)
+    const db = client.db('task-manager')
     const users = db.collection('users')
+
+    const id = new ObjectId()
+    console.log(id)
+    console.log(id.getTimestamp())
+    console.log(id.id)
+    console.log(id.id.length)
+    console.log(id.toHexString().length)
 
     var resultUsers = await users.insertMany([{ name: 'Isha', age: 15 }, { name: 'Akshita', age: 15}])
     console.log(resultUsers)
@@ -21,11 +27,38 @@ async function connectToDBServer(){
         {description: 'Solve 3 LeetCode Problems', completed: true}, 
         {description: 'Take a Node.js refresher', completed: false}
     ])
-    console.log(resultTasks)
+    console.log(resultTasks) 
+    
 
     await client.close()
 }
 
-connectToDBServer().catch( error => {console.log('Insertion failed', error)})
+async function findDocs(){
+    await client.connect()
+    await client.db("admin").command({ ping: 1 })
+    console.log('Connection established and tested')  
+    
+    const db = client.db('task-manager')
+    const users = db.collection('users')
+    const tasks = db.collection('tasks')
+
+    var user = await users.findOne({ age: 15 })
+    var userData = await users.find({ age: 20 }).toArray()
+
+    var userAge20Count = await users.countDocuments({ age:20 })
+    var userCount = await users.estimatedDocumentCount()
+
+    var task = await tasks.findOne({ _id: new ObjectId("653bea02afb2b8e8a6e6188c")})
+    console.log("Last task: ", task)
+
+    var tasksNotCompleted = await tasks.find({ completed: false}).toArray()
+    console.log("Tasks that are not completed: ", tasksNotCompleted)
+
+    await client.close()
+}
+
+//insertDocs().catch( error => {console.log('Insertion failed', error)})
+
+findDocs().catch( error => {console.log('Fetching failed', error)})
 
 
