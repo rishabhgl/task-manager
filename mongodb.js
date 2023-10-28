537b6da33bc4ebeddafc98139ade02e012f82981
rishabhgl <rishabhisjayden@gmail.com>
2023-10-28 17:54:53 +0530

    Insterted docs

--- mongodb.js
+++ mongodb.js
@@ -7,13 +7,25 @@ const dbName = 'task-manager'
 async function connectToDBServer(){
     await client.connect()
     await client.db("admin").command({ ping: 1 })
-    console.log('Connection established and tested')
+    console.log('Connection established and tested')  
+    
+    const db = client.db(dbName)
+    const users = db.collection('users')
+
+    var resultUsers = await users.insertMany([{ name: 'Isha', age: 15 }, { name: 'Akshita', age: 15}])
+    console.log(resultUsers)
+
+    const tasks = db.collection('tasks')
+    var resultTasks = await tasks.insertMany([ 
+        {description: 'Machine Learning Project', completed: false}, 
+        {description: 'Solve 3 LeetCode Problems', completed: true}, 
+        {description: 'Take a Node.js refresher', completed: false}
+    ])
+    console.log(resultTasks)
+
+    await client.close()
 }
 
-connectToDBServer().catch(() => {console.log('Connection failed')})
+connectToDBServer().catch( error => {console.log('Insertion failed', error)})
 
-const db = client.db(dbName)
-db.collection('users').insertOne({
-    name: 'Rishabh',
-    age: 19
-})
+
