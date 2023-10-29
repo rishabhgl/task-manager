350993a86542051699897b2b5eb5154b584c0951
rishabhgl <rishabhisjayden@gmail.com>
2023-10-29 12:24:26 +0530

    Added schema validation

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose')
+const validator = require('validator')
 
 const serverURL = "mongodb://127.0.0.1:27017"
 
@@ -8,25 +9,54 @@ async function main(){
 
     const User = mongoose.model('User', {
         name: {
-            type: String
+            type: String,
+            required: true,
+            trim: true,
+        },
+        email: {
+            type: String,
+            required: true,
+            trim: true,
+            lowercase: true,
+            validate(value){
+                if (!validator.isEmail(value)) throw new Error('Email is invalid!!')
+            }
+        },
+        password:{
+            type: String,
+            required: true,
+            minlength: 6,
+            trim: true,
+            validate(value){
+                if (value.toLowerCase().includes("password")) throw new Error('Please enter a stronger password!')
+            }
         },
         age: {
-            type: Number
+            type: Number,
+            default: 0,
+            min: 0
         }
     })
 
     const Task = mongoose.model('Task', {
         description: {
-            type: String
+            type: String,
+            required: true,
+            trim: true
         },
         completed: {
-            type: Boolean
+            type: Boolean,
+            default: false
         }
     })
 
-    var user_inst = new User({ name: 'Rishabh', age: 19})
-    var task_inst = new Task({ description: 'Completing Software Engineering Project', completed: false})
-    await task_inst.save()
+    var user_inst = new User({ name: 'Varun  ', email: 'IRHSJIC@NIC.ORG', password: ' sword', age: 19})
+
+    const result = await user_inst.save()
+    console.log(result)
+
+    var task_inst = new Task({ description: 'Completing Software Engineering Project'})
+
 }
 
 main().catch(err => {console.log(err)})
