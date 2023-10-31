8f5e522a70e8933918a776fbc8c91af547ec9320
rishabhgl <rishabhisjayden@gmail.com>
2023-10-31 19:20:23 +0530

    Set up delete and update task, user routes

--- src/index.js
+++ src/index.js
@@ -80,6 +80,63 @@ app.get('/tasks/:id', async (req, res) => {
     
 })
 
+app.patch('/users/:id', async (req, res) => {
+
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['name', 'age', 'email', 'password']
+    const isValidUpdate = updates.every(update => allowedUpdates.includes(update))
+
+    if (!isValidUpdate) return res.status(400).send({ error: 'Invalid update!'})
+
+    try{
+        const updatedUser = await User.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true}) 
+        if (!updatedUser) return res.status(404).send({ error: 'Could not find user!' })
+        res.send(updatedUser)
+    } catch(e){
+        res.status(400).send(e)
+        console.log(e)
+    }
+
+})
+
+app.patch('/tasks/:id', async (req, res) => {
+
+    const updates = Object.keys(req.body)
+    const allowedUpdates = ['description', 'completed']
+    const isValidUpdate = updates.every(update => allowedUpdates.includes(update))
+
+    if (!isValidUpdate) return res.status(400).send({ error: 'Invalid update!'})
+
+    try{
+        const updatedTask = await Task.findByIdAndUpdate(req.params.id, req.body, { new: true, runValidators: true}) 
+        if (!updatedTask) return res.status(404).send({ error: 'Could not find task!' })
+        res.send(updatedTask)
+    } catch(e){
+        res.status(400).send(e)
+    }
+
+})
+
+app.delete('/users/:id', async (req, res) => {
+    try{
+        const user = await User.findByIdAndDelete(req.params.id)
+        if (!user) res.status(404).send({ error: 'User not found!'})
+        res.send(user)
+    } catch(e){
+        res.status(500).send()
+    }
+})
+
+app.delete('/tasks/:id', async (req, res) => {
+    try{
+        const task = await Task.findByIdAndDelete(req.params.id)
+        if (!task) res.status(404).send({ error: 'Task not found!'})
+        res.send(task)
+    } catch(e){
+        res.status(500).send()
+    }
+})
+
 app.listen(port, () => {
     console.log('Server running on port ' + port + '!!')
 })
